b93e260f273a114c1dcd511ae7447cdd5218384a
DWM_Founder <32415129+TSPFounder@users.noreply.github.com>
2026-06-23 14:50:36 -0400

    Add WorldPackageExporter; export test package button on dashboard

--- DWMStudio/ViewModels/DashboardViewModel.cs
+++ DWMStudio/ViewModels/DashboardViewModel.cs
@@ -6,12 +6,15 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using DWMStudio.Models;
+using DWM.Shared;
 
 namespace DWMStudio.ViewModels
 {
     public sealed partial class DashboardViewModel : ObservableObject
     {
         public ObservableCollection<WorldProject> Worlds { get; } = new();
+
+        [ObservableProperty] private string _statusMessage = string.Empty;
 
         public DashboardViewModel()
         {
@@ -33,6 +36,15 @@ namespace DWMStudio.ViewModels
         [RelayCommand]
         private void OpenNewWorldWizard() =>
             WeakReferenceMessenger.Default.Send(new OpenWizardMessage());
+
+        [RelayCommand]
+        private void ExportTestPackage()
+        {
+            var exporter = new DWM.Shared.WorldPackageExporter();
+            exporter.WriteHardcodedPendulum(
+                @"C:\DreamWorldMaker\Packages\DWM_WorldPackage_pendulum.db");
+            StatusMessage = "Package written to C:\\DreamWorldMaker\\Packages\\";
+        }
 
         [RelayCommand]
         private void OpenWorld(WorldProject? world)
